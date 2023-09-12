bb0e2634c9183936acb93596a7de3f0d1b1e920c
brvhjustice <62227490+brvhjustice@users.noreply.github.com>
2023-09-12 23:55:43 +0000

    Initial commit

---
+++ Scripts/MoveForwardX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForwardX : MonoBehaviour
+{
+    public float speed;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = new Vector3(0, 0, 0);
+      //Vector3 spawnPos = Vector3();
+        transform.Translate(Vector3.forward * speed * Time.deltaTime );
+        
+    }
+}
